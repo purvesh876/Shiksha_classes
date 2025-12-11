aca37f3bfc8bfaaf3ee6e7ce5c2e2278ff1f0f85
Anjney Lawaniya <anjneylawaniya@gmail.com>
2025-12-11 14:26:23 +0530

    .

--- src/components/SucStr.tsx
+++ src/components/SucStr.tsx
@@ -6,9 +6,31 @@ import Eshika from "../assets/images/Eshika.jpg";
 import Kanay from "../assets/images/Kanay.jpg";
 import Piyush from "../assets/images/Piyush.jpg";
 import Purvesh from "../assets/images/Purvesh.jpg";
+import { useEffect } from "react";
 
 
 export default function SucStr(){
+
+    useEffect(() => {
+    const slides = document.querySelectorAll<HTMLDivElement>(".slide");
+
+    slides.forEach(slide => {
+      const add = () => slide.classList.add("enlarge-touch");
+      const remove = () => slide.classList.remove("enlarge-touch");
+
+      slide.addEventListener("touchstart", add);
+      slide.addEventListener("touchend", remove);
+      slide.addEventListener("touchcancel", remove);
+
+      // Cleanup on unmount
+      return () => {
+        slide.removeEventListener("touchstart", add);
+        slide.removeEventListener("touchend", remove);
+        slide.removeEventListener("touchcancel", remove);
+      };
+    });
+  }, []);
+
     return (
         <>
             <h2 className="result-heading">RESULTS</h2>
